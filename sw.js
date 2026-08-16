e3ee7c3e3d9a4f1b9266f651c29adee7bdf3a1fa
bohdan16 <43153912+bohdan16@users.noreply.github.com>
2026-08-16 09:16:48 -0400

    Update service worker cache version to v3.3.0

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE='liftlog-v3.2.1';
+const CACHE='liftlog-v3.3.0';
 const ASSETS=['./','./index.html','./styles.css','./app.js','./manifest.webmanifest','./icon-192.png'];
 self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS)).then(()=>self.skipWaiting())));
 self.addEventListener('activate',e=>e.waitUntil(caches.open(CACHE).then(c=>caches.keys().then(keys=>Promise.all(keys.map(k=>k!==CACHE&&caches.delete(k))))).then(()=>self.clients.claim())));
